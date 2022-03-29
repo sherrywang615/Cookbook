366fceac5ffebcd3b2cb5fda37452ab28546f235
chriswoojinlee <chrislee707@gmail.com>
2022-03-28 19:29:50 -0700

    Fixed parent key violation bugs

--- CreateTables.sql
+++ CreateTables.sql
@@ -4,58 +4,48 @@ DROP TABLE Recipe_2;
 DROP TABLE Recipe_3;
 DROP TABLE Requires_1;
 DROP TABLE Ingredient;
-DROP TABLE Has_1;
 
 CREATE TABLE Users (
 userID CHAR(30),
 userName CHAR(20),
-password CHAR(20),
+userPassword CHAR(20),
 PRIMARY KEY(userID),
-UNIQUE (userName))
+UNIQUE (userName));
 
-CREATE TABLE Recipe_1(
-recipeID CHAR(30),
-recipeName CHAR(20),
-PRIMARY KEY (recipeID),
-FOREIGN KEY (recipeName) REFERENCES Recipe_2
-ON DELETE CASCADE)
+CREATE TABLE Recipe_3(
+preparationTime INTEGER,
+difficulty INTEGER,
+PRIMARY KEY(preparationTime));
 
 CREATE TABLE Recipe_2 (
 recipeName CHAR(20),
 preparationTime INTEGER,
 PRIMARY KEY(recipeName),
 FOREIGN KEY (preparationTime) REFERENCES Recipe_3
-ON DELETE CASCADE)
+ON DELETE CASCADE);
 
-CREATE TABLE Recipe_3(
-preparationTime INTEGER,
-difficulty INTEGER,
-PRIMARY KEY(preparationTime))
-
-CREATE TABLE Requires_1 ( 
-recipeID INTEGER,
-ingredientID INTEGER,
-PRIMARY KEY (recipeID, ingredientID),
-FOREIGN KEY (ingredientID) REFERENCES Ingredient
-ON DELETE CASCADE,
-FOREIGN KEY (recipeID) REFERENCES Recipe
-ON DELETE CASCADE)
+CREATE TABLE Recipe_1(
+recipeID CHAR(30),
+recipeName CHAR(20),
+PRIMARY KEY (recipeID),
+FOREIGN KEY (recipeName) REFERENCES Recipe_2
+ON DELETE CASCADE);
 
 CREATE TABLE Ingredient (
 ingredientID CHAR(30), 
 ingredientName CHAR(20), 
 amount INTEGER, 
 unit CHAR(20),
-PRIMARY KEY (ingredientID))
+PRIMARY KEY (ingredientID));
 
-CREATE TABLE Has_1 (
-userID CHAR(30),
+CREATE TABLE Requires_1 ( 
 recipeID CHAR(30),
-PRIMARY KEY (userID, recipeID),
-FOREIGN KEY (userID) REFERENCES Users
+ingredientID CHAR(30),
+PRIMARY KEY (recipeID, ingredientID),
+FOREIGN KEY (ingredientID) REFERENCES Ingredient,
 ON DELETE CASCADE,
-FOREIGN KEY (recipeID) REFERENCES Recipe
-ON DELETE CASCADE)
+FOREIGN KEY (recipeID) REFERENCES Recipe_1
+ON DELETE CASCADE);
 
 
 INSERT INTO Users VALUES ('user_1', 'qwe', '123');
@@ -64,23 +54,23 @@ INSERT INTO Users VALUES ('user_3', 'zxc', '12345');
 INSERT INTO Users VALUES ('user_4', 'rty', '123456');
 INSERT INTO Users VALUES ('user_5', 'vbn', '1234567');
 
-INSERT INTO Recipe_1 VALUES ('rec_1', 'Bread');
-INSERT INTO Recipe_1 VALUES ('rec_2', 'Pizza');
-INSERT INTO Recipe_1 VALUES ('rec_3', 'Cake');
-INSERT INTO Recipe_1 VALUES ('rec_4', 'Taco');
-INSERT INTO Recipe_1 VALUES ('rec_5', 'Noodles');
-
-INSERT INTO Recipe_2 VALUES ('Bread', '40');
-INSERT INTO Recipe_2 VALUES ('Piazza', '30');
-INSERT INTO Recipe_2 VALUES ('Mango Cake', '50');
-INSERT INTO Recipe_2 VALUES ('Banana Smoothie', '10');
-INSERT INTO Recipe_2 VALUES ('Noodles', '20');
-
 INSERT INTO Recipe_3 VALUES ('10', '1');
 INSERT INTO Recipe_3 VALUES ('30', '3');
 INSERT INTO Recipe_3 VALUES ('50', '5');
 INSERT INTO Recipe_3 VALUES ('20', '2');
 INSERT INTO Recipe_3 VALUES ('40', '4');
+
+INSERT INTO Recipe_2 VALUES ('Bread', '40');
+INSERT INTO Recipe_2 VALUES ('Pizza', '30');
+INSERT INTO Recipe_2 VALUES ('Mango Cake', '50');
+INSERT INTO Recipe_2 VALUES ('Banana Smoothie', '10');
+INSERT INTO Recipe_2 VALUES ('Noodles', '20');
+
+INSERT INTO Recipe_1 VALUES ('rec_1', 'Bread');
+INSERT INTO Recipe_1 VALUES ('rec_2', 'Pizza');
+INSERT INTO Recipe_1 VALUES ('rec_3', 'Mango Cake');
+INSERT INTO Recipe_1 VALUES ('rec_4', 'Banana Smoothie');
+INSERT INTO Recipe_1 VALUES ('rec_5', 'Noodles');
 
 INSERT INTO Requires_1 VALUES ('rec_1', 'ing_1');
 INSERT INTO Requires_1 VALUES ('rec_1', 'ing_9');
@@ -100,7 +90,7 @@ INSERT INTO Requires_1 VALUES ('rec_4', 'ing_5');
 INSERT INTO Requires_1 VALUES ('rec_4', 'ing_2');
 
 INSERT INTO Requires_1 VALUES ('rec_5', 'ing_8');
-INSERT INTO Requires_1 VALUES ('rec_4', 'ing_5');
+INSERT INTO Requires_1 VALUES ('rec_5', 'ing_5');
 
 INSERT INTO Ingredient VALUES ('ing_1', 'Flour', '400', 'g');
 INSERT INTO Ingredient VALUES ('ing_2', 'Banana', '200', 'g');
